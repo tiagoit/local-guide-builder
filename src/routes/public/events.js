7e0f141212c32f19cafe98b92cdf597e5fd9038e
Tiago Brito <otiagobrito@gmail.com>
2019-03-29 10:30:38 -0300

    events page title

--- src/routes/public/events.js
+++ src/routes/public/events.js
@@ -7,12 +7,21 @@ const {City} = require('../../models/city');
 
 router.get('/:cityCode?/:orgCode?/:eventCode?', async function(req, res) {
   try {
-      const cities = await City.find();
-      let { events, featured } = await eventsService.getEvents(req.params['cityCode'], req.params[' orgCode'], req.params['eventCode']);
-      res.render('./pages/events/events', {events, featured, cities, moment, utilsService});
+    const cities = await City.find();
+    let { events, featured } = await eventsService.getEvents(req.params['cityCode'], req.params[' orgCode'], req.params['eventCode']);
+
+    let activeCity = undefined;
+    if(req.params['cityCode']) {
+      cities.forEach((city) => {
+        if(city.code === req.params['cityCode']) activeCity = city.name;
+      });
+
+    }
+
+    res.render('./pages/events/events', {events, featured, cities, activeCity, moment, utilsService});
   } catch (ex) {
-      console.log('EXCEPTION: ', ex);
-      return res.status(404).send(ex);
+    console.log('EXCEPTION: ', ex);
+    return res.status(404).send(ex);
   }
 });
 
