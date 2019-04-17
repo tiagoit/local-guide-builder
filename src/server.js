19adab03931b86513d34ed98865eb145a1922005
Tiago Brito <otiagobrito@gmail.com>
2019-04-17 09:34:45 -0300

    internationalization

--- src/server.js
+++ src/server.js
@@ -6,9 +6,10 @@ const cors = require('cors');
 const mongoose = require('mongoose'); 
 const expressJwt = require('express-jwt');
 const config = require('config');
+const session = require('express-session')
+const i18n = require('i18n-express');
 
 const moment = require('moment-timezone');
-moment.locale('pt-BR');
 moment.tz.setDefault('America/Sao_Paulo');
 
 const DEBUG = process.env.NODE_ENV !== 'production';
@@ -20,6 +21,27 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../public'));
+
+app.use(session({
+    secret: 'theWildRunnerDropedSomeMushroomCake0nTheRoad',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+app.use(i18n({
+    translationsPath: path.join(__dirname, 'i18n'),
+    siteLangs: ['en', 'es', 'pt'],
+    defaultLocale: 'pt',
+    textsVarName: 'translation'
+}));
+
+// set locale based on session.ulang
+app.use(function (req, res, next) {
+    moment.locale(req.session.ulang);
+    next();
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
@@ -40,6 +62,7 @@ app.use('/api/migrations', require('./routes/api/migrations.api'));
 app.use('/', require('./routes/public/index.pub'));
 app.use('/eventos', require('./routes/public/events.pub'));
 
+
 app.get('/eita', (req, res) => {
     res.render('./pages/eita', {env: config.get('env'), moment: moment});
 });
@@ -54,7 +77,10 @@ app.use(function (err, req, res, next) {
     }
 });
 
+
+// // TODO: Error logging on StackDriver / create alerts for production errors on GCP
 // app.use(function(err, req, res, next) {
+//     console.log('############################### ERROR: ', err);
 //     if(err.name === 'UnauthorizedError') {
 //       // res.status(err.status).render('./pages/eita', {env: config.get('env'), moment: moment});
 //     //   res.render('./pages/events/events-list', { events, cities, tags, appliedFilters, moment, appService, env });
